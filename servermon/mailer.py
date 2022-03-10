1bd7cddecaf82db486502afd0adc71a99d95ffcc
Renee Louise Brinkman <reneebrinkman91@gmail.com>
2022-03-10 10:34:00 -0600

    Add functionality for SMTP authentication and sending basic downtime alerts

--- servermon/mailer.py
+++ servermon/mailer.py
@@ -4,9 +4,26 @@ contains functionality to send mail about server status to the admin
 """
 
 from smtplib import SMTP_SSL
+from email.message import Message
+import datetime
 import conf
 
 
-def noop():
-    with SMTP_SSL(conf.SMTP_SERVER) as smtp:
-        return smtp.noop()
+def noop(smtp_object):
+    return smtp_object.noop()
+
+
+def login(smtp_object):
+    return smtp_object.login(conf.SMTP_USER, conf.SMTP_PASSWORD)
+
+
+def send_alert(smtp_object, body_text):
+    message = Message()
+    message['Resent-Date'] = str(datetime.datetime.now())
+    message['Subject'] = 'ALERT: Server Down!'
+    message.set_payload(body_text)
+
+    return smtp_object.sendmail(
+        conf.FROM_ADDRESS,
+        conf.EMAIL_ADDRESS,
+        message.as_string())
--- servermon/tests/test_mailer.py
+++ servermon/tests/test_mailer.py
@@ -1,5 +1,29 @@
+from smtplib import SMTP_SSL
 import pytest
+import conf
 from servermon import mailer
 
-def test_noop():
-    assert mailer.noop()[0] == 250
+
+@pytest.fixture(scope='module')
+def smtp():
+    smtp_object = SMTP_SSL(conf.SMTP_SERVER)
+    yield smtp_object
+    smtp_object.quit()
+
+
+@pytest.fixture
+def authenticated_smtp(smtp):
+    mailer.login(smtp)
+    return smtp
+
+
+def test_noop(smtp):
+    assert mailer.noop(smtp)[0] == 250
+
+
+def test_login(smtp):
+    assert mailer.login(smtp)[0] == 235
+
+
+def test_send_alert(authenticated_smtp):
+    assert mailer.send_alert(authenticated_smtp, 'Test Alert') == {}
