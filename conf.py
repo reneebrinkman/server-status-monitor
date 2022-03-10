4282f98dae0d56f4232b3f3d3bc99a1684afd314
Renee Louise Brinkman <reneebrinkman91@gmail.com>
2022-03-10 09:09:25 -0600

    Add more configuration options

--- conf.py
+++ conf.py
@@ -9,6 +9,13 @@ EMAIL_ADDRESS = 'reneebrinkman91@gmail.com'
 # your server for sending email
 SMTP_SERVER = 'smtp.gmail.com'
 
+# if SMTP server requires authentication
+SMTP_USER = 'servermon2022'
+SMTP_PASSWORD = 'changeme'
+
+# email address to send from
+FROM_ADDRESS = 'servermon2022@gmail.com'
+
 # These are the servers the app should check.
 # Each entry in the list takes the following form:
 # tuple(hostname, port)
